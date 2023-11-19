26832bd7e91780c45401c275f6401163098d9033
LyzaFortes <lyzas2jr@gmail.com>
2023-11-19 13:03:17 -0300

    Criação página inicial(clone)

---
+++ src/screens/Inicial/style.jsx
@@ -0,0 +1,67 @@
+import { StyleSheet } from "react-native";
+
+ export const styles = StyleSheet.create({
+    tudo: {
+      flex: 1,
+      backgroundColor: '#050505',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+   containerLogo:{
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: 50,
+   },
+   containerEntrar:{
+    flex: 0.6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%',
+   },
+   btnEntrarN:{
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    width:'100%',
+    padding: 15,
+   },
+   textoN:{
+    color: '#050505',
+    textAlign:'center',
+   },
+   containerInstrucao:{
+    paddingHorizontal: 4
+   },
+   instrucao:{
+    color: '#acabb0',
+    textAlign: 'center',
+    marginTop: 8,
+    fontSize: 13.8,
+   },
+   btnEntrarE:{
+    backgroundColor:'#201f24',
+    borderRadius: 5,
+    width:'100%',
+    padding: 15,
+    marginTop: 40,
+   },
+   textoE:{
+    color:'#fff',
+    textAlign: 'center',
+   },
+   termos:{
+    color: '#acabb0',
+    textAlign: 'center',
+    marginTop: 16,
+    fontSize: 13.8,
+   },
+   textoLink:{
+    color:'#5d82bc',
+    textDecorationLine: 'underline',
+   },
+   problemas:{
+    marginVertical: 25,
+   }
+   
+
+
+  })
